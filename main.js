a92631fe83dcb9d91e1e07fdf595f80fc2c7261f
mohamed <mohamedeng747@gmail.com>
2026-01-28 17:19:29 +0200

    message

---
+++ main.js
@@ -0,0 +1,19 @@
+const navbar = document.querySelector(".navbar");
+const bars = document.querySelector(".fa-bars");
+const xmark = document.querySelector(".fa-xmark");
+const humburgerMenu = document.querySelector(".humburger");
+
+humburgerMenu.addEventListener("click", () => {
+  bars.classList.toggle("active");
+  xmark.classList.toggle("active");
+  navbar.classList.toggle("active");
+});
+
+// دي عشان تقفل  المنيو
+document.querySelectorAll(".navbar ul li a").forEach(link => {
+    link.addEventListener("click", () => {
+        navbar.classList.remove("active");
+        bars.classList.add("active");     
+        xmark.classList.remove("active");  
+    });
+});
